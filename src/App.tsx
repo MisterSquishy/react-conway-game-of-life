15258396d6fda81005571563ae46b78c4b79e238
MisterSquishy <pdavids@comcast.net>
2022-02-05 22:45:11 -0500

    tracing?

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { context, trace } from "@opentelemetry/api";
+import { CollectorTraceExporter } from "@opentelemetry/exporter-collector";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
 import { Button, InputNumber, Layout, PageHeader, Select, Space } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import produce from "immer";
 import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
 import SHAPES, { CELL_STATE, PREFABS } from "./shapes";
+
 const { Option, OptGroup } = Select;
+
+// Create a provider for activating and tracking spans
+const tracerProvider = new WebTracerProvider();
+
+// Connect to Lightstep by configuring the exporter with your endpoint and access token.
+tracerProvider.addSpanProcessor(
+  new BatchSpanProcessor(
+    new CollectorTraceExporter({
+      url: "https://ingest.lightstep.com:443/api/v2/otel/trace",
+      headers: {
+        "Lightstep-Access-Token":
+          "EcBjBBDRmEx/0rSEEjwaDmu17kRjCNYDoErvoqsLSd+/ZpOE0tWr6cv/wVW5+WShxA+cg/f1T6t2gsnhjqFStbuiwhnfFf0LirNMRyr9",
+      },
+    })
+  )
+);
+
+// Register the tracer
+tracerProvider.register();
+trace.setGlobalTracerProvider(tracerProvider);
+const name = "game of life";
+const version = "0.1.0";
+const tracer = trace.getTracer(name, version);
 
 const aliveColor = "#007EA6";
 const starvingColor = "#88A990";
@@ -38,38 +66,52 @@ const operations = [
   [-1, 0],
 ];
 
-const generateEmptyGrid = (): Grid => {
-  const rows = [];
-  for (let i = 0; i < numRows; i++) {
-    rows.push(
-      Array.from(Array(numCols), () => ({
-        previous: CELL_STATE.DEAD,
-        current: CELL_STATE.DEAD,
-        next: CELL_STATE.DEAD,
-      }))
-    );
-  }
-
-  return rows;
-};
-
-const mergeGrids = (grid1: Grid, grid2: Grid): Grid => {
-  const mergedGrid: Grid = [];
-  for (var i = 0; i < numRows; i++) {
-    mergedGrid[i] = [];
-    for (var k = 0; k < numCols; k++) {
-      mergedGrid[i][k] = grid1[i]?.[k] || grid2[i]?.[k];
-    }
-  }
-  return mergedGrid;
-};
-
 const App: React.FC = () => {
   const [rulesModalOpen, setRulesModalOpen] = useState(false);
   const [hoveredCell, setHoveredCell] = useState<[number, number]>();
   const [cursorType, setCursorType] = useState<PREFABS>(PREFABS.POINT);
   const intervalMs = useRef(50);
   const generation = useRef(0);
+  const renderSpan = tracer.startSpan("render frame", {
+    attributes: {
+      generation: generation.current,
+      intervalMs: intervalMs.current,
+    },
+  });
+
+  const generateEmptyGrid = useCallback((): Grid => {
+    const ctx = trace.setSpan(context.active(), renderSpan);
+    const emptyGridSpan = tracer.startSpan("generate empty grid", {}, ctx);
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(
+        Array.from(Array(numCols), () => ({
+          previous: CELL_STATE.DEAD,
+          current: CELL_STATE.DEAD,
+          next: CELL_STATE.DEAD,
+        }))
+      );
+    }
+    emptyGridSpan.end();
+    return rows;
+  }, [renderSpan]);
+
+  const mergeGrids = useCallback(
+    (grid1: Grid, grid2: Grid): Grid => {
+      const ctx = trace.setSpan(context.active(), renderSpan);
+      const mergeGridsSpan = tracer.startSpan("merge grids", {}, ctx);
+      const mergedGrid: Grid = [];
+      for (var i = 0; i < numRows; i++) {
+        mergedGrid[i] = [];
+        for (var k = 0; k < numCols; k++) {
+          mergedGrid[i][k] = grid1[i]?.[k] || grid2[i]?.[k];
+        }
+      }
+      mergeGridsSpan.end();
+      return mergedGrid;
+    },
+    [renderSpan]
+  );
 
   const [grid, setGrid] = useState(() => {
     return generateEmptyGrid();
@@ -80,7 +122,9 @@ const App: React.FC = () => {
   const runningRef = useRef(running);
   runningRef.current = running;
 
-  const runSimulation = useCallback(() => {
+  const runGeneration = useCallback(() => {
+    const ctx = trace.setSpan(context.active(), renderSpan);
+    const runGenerationSpan = tracer.startSpan("run generation", {}, ctx);
     if (!runningRef.current) {
       return;
     }
@@ -89,6 +133,12 @@ const App: React.FC = () => {
     setGrid((g) => {
       return produce(g, (gridCopy) => {
         // set previous and current
+        const ctx = trace.setSpan(context.active(), runGenerationSpan);
+        const generateCurrentGenerationSpan = tracer.startSpan(
+          "generate current generation",
+          {},
+          ctx
+        );
         for (let i = 0; i < numRows; i++) {
           for (let k = 0; k < numCols; k++) {
             gridCopy[i][k].previous = gridCopy[i][k].current;
@@ -113,6 +163,12 @@ const App: React.FC = () => {
             }
           }
         }
+        generateCurrentGenerationSpan.end();
+        const generateNextGenerationSpan = tracer.startSpan(
+          "generate next generation",
+          {},
+          ctx
+        );
         // set next
         for (let i = 0; i < numRows; i++) {
           for (let k = 0; k < numCols; k++) {
@@ -142,69 +198,89 @@ const App: React.FC = () => {
             }
           }
         }
+        generateNextGenerationSpan.end();
       });
     });
 
-    setTimeout(runSimulation, intervalMs.current);
-  }, [generation, intervalMs]);
+    setTimeout(runGeneration, intervalMs.current);
+    runGenerationSpan.end();
+  }, [generation, intervalMs, renderSpan]);
 
-  const getCellsForCursor = (i: number, k: number): Grid => {
-    const cursorShape = SHAPES[cursorType];
-    const newGrid: Grid = [];
-    var startRow = i;
-    var startCol = k;
-    if (numRows < i + cursorShape.length) {
-      // shape would run off bottom of grid
-      startRow = numRows - cursorShape.length;
-    }
-    if (numCols < k + cursorShape[0].length) {
-      // shape would run off right of grid
-      startCol = numCols - cursorShape[0].length;
-    }
-    for (var row = 0; row < cursorShape.length; row++) {
-      newGrid[startRow + row] = [];
-      for (var col = 0; col < cursorShape[row].length; col++) {
-        newGrid[startRow + row][startCol + col] = {
-          previous: cursorShape[row][col],
-          current: cursorShape[row][col],
-          next: cursorShape[row][col],
-        };
+  const getCellsForCursor = useCallback(
+    (i: number, k: number): Grid => {
+      const ctx = trace.setSpan(context.active(), renderSpan);
+      const getCellsForCursorSpan = tracer.startSpan(
+        "get cells for cursor",
+        {},
+        ctx
+      );
+      const cursorShape = SHAPES[cursorType];
+      const newGrid: Grid = [];
+      var startRow = i;
+      var startCol = k;
+      if (numRows < i + cursorShape.length) {
+        // shape would run off bottom of grid
+        startRow = numRows - cursorShape.length;
       }
-    }
-    return newGrid;
-  };
+      if (numCols < k + cursorShape[0].length) {
+        // shape would run off right of grid
+        startCol = numCols - cursorShape[0].length;
+      }
+      for (var row = 0; row < cursorShape.length; row++) {
+        newGrid[startRow + row] = [];
+        for (var col = 0; col < cursorShape[row].length; col++) {
+          newGrid[startRow + row][startCol + col] = {
+            previous: cursorShape[row][col],
+            current: cursorShape[row][col],
+            next: cursorShape[row][col],
+          };
+        }
+      }
+      getCellsForCursorSpan.end();
+      return newGrid;
+    },
+    [cursorType, renderSpan]
+  );
 
-  const cellColorStyle = (i: number, k: number) => {
-    const cellState = grid[i][k];
-    const [hoveredRow, hoveredColumn] = hoveredCell || [];
-    const hoveredCells =
-      hoveredRow !== undefined && hoveredColumn !== undefined
-        ? getCellsForCursor(hoveredRow, hoveredColumn)
-        : [];
-    if (
-      hoveredRow !== undefined &&
-      hoveredColumn !== undefined &&
-      hoveredCells[i] &&
-      hoveredCells[i][k]?.current === CELL_STATE.ALIVE
-    ) {
-      return {
-        backgroundColor: hoverColor,
-      };
-    } else if (cellState.current === CELL_STATE.ALIVE) {
-      return {
-        backgroundColor: aliveColor,
-      };
-    } else if (cellState.current === CELL_STATE.DEAD) {
+  const cellColorStyle = useCallback(
+    (i: number, k: number) => {
+      const ctx = trace.setSpan(context.active(), renderSpan);
+      const cellColorStyleSpan = tracer.startSpan("cell color style", {}, ctx);
+      const cellState = grid[i][k];
+      const [hoveredRow, hoveredColumn] = hoveredCell || [];
+      const hoveredCells =
+        hoveredRow !== undefined && hoveredColumn !== undefined
+          ? getCellsForCursor(hoveredRow, hoveredColumn)
+          : [];
       if (
-        cellState.previous !== CELL_STATE.DEAD ||
-        cellState.next !== CELL_STATE.DEAD
+        hoveredRow !== undefined &&
+        hoveredColumn !== undefined &&
+        hoveredCells[i] &&
+        hoveredCells[i][k]?.current === CELL_STATE.ALIVE
       ) {
         return {
-          backgroundColor: starvingColor,
+          backgroundColor: hoverColor,
         };
+      } else if (cellState.current === CELL_STATE.ALIVE) {
+        return {
+          backgroundColor: aliveColor,
+        };
+      } else if (cellState.current === CELL_STATE.DEAD) {
+        if (
+          cellState.previous !== CELL_STATE.DEAD ||
+          cellState.next !== CELL_STATE.DEAD
+        ) {
+          return {
+            backgroundColor: starvingColor,
+          };
+        }
       }
-    }
-  };
+      cellColorStyleSpan.end();
+    },
+    [getCellsForCursor, grid, hoveredCell, renderSpan]
+  );
+
+  renderSpan.end();
 
   return (
     <Layout
@@ -245,7 +321,7 @@ const App: React.FC = () => {
               setRunning(!running);
               if (!running) {
                 runningRef.current = true;
-                runSimulation();
+                runGeneration();
               }
             }}
           >
@@ -331,8 +407,15 @@ const App: React.FC = () => {
               <div
                 key={`${i}-${k}`}
                 onClick={() => {
+                  const ctx = trace.setSpan(context.active(), renderSpan);
+                  const gridCellClickSpan = tracer.startSpan(
+                    "grid cell click",
+                    {},
+                    ctx
+                  );
                   const clickedCells = getCellsForCursor(i, k);
                   setGrid(mergeGrids(clickedCells, grid));
+                  gridCellClickSpan.end();
                 }}
                 onMouseEnter={() => setHoveredCell([i, k])}
                 onMouseLeave={() => setHoveredCell(undefined)}
